376d8fa5d9f20563d705ae103234f2bc4df382fa
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-09-03 21:16:29 +0200

    fix #2

--- eventswithoutbattles.js
+++ eventswithoutbattles.js
@@ -27,6 +27,7 @@ const client = new Client({ context, queryEngine: new QueryEngineComunica(comuni
 const queryAllEvents = `
   query {
     id @single
+    name @single
     type(_:DanceEvent)
   }`;
 
@@ -42,19 +43,12 @@ main();
 async function main() {
   // Execute the query
   const data = await executeQuery(queryAllEvents);
-  console.log(data);
-  const allEventIDs = data.map(d => d.id);
   const data2 = await executeQuery(queryEventsWithBattle);
-  console.log(data2);
-  const eventsWithBattleIDs = data2.map(d => d.id);
-  const filtered = allEventIDs.filter(id => eventsWithBattleIDs.indexOf(id) === -1);
-  console.log(filtered);
-  //let result = filterLevel(data);
-  //result = filterAge(result);
-  //result = filterDates(result, new Date('2019-04-01'), new Date('2019-06-30'));
-  //console.log(result);
 
-  //printAsCSV(result);
+  const eventsWithBattleIDs = data2.map(d => d.id);
+  const filtered = data.filter(event => eventsWithBattleIDs.indexOf(event.id) === -1);
+
+  printAsCSV(filtered);
 }
 
 async function executeQuery(query){
@@ -63,22 +57,10 @@ async function executeQuery(query){
   return data;
 }
 
-function filterDates(dancers, startDate, endDate) {
-  dancers.forEach(dancer => {
-    //console.log(dancer.wins);
-    dancer.wins = dancer.wins.filter(battle => (new Date(battle.start)) >= startDate && (new Date(battle.end)) <= endDate).length;
-  });
-
-  // TODO put this in separate method.
-  dancers = dancers.filter(dancer => dancer.wins > 0);
-
-  return dancers;
-}
-
 function printAsCSV(data) {
-  console.log(`dancer,wins`);
+  console.log(`id,name`);
 
-  data.forEach(dancer => {
-    console.log(`${dancer.name},${dancer.wins}`);
+  data.forEach(event => {
+    console.log(`"${event.id}","${event.name}"`);
   });
 }
